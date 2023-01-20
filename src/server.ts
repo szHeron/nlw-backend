68cebdf8f3e3979df17ed517881a31515eb51fbe
szheron <heronbr7@gmail.com>
2023-01-20 09:48:12 -0300

    adicionado as primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import { AppRoutes } from './routes';
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/', async ()=>{
-    const habits = await prisma.habit.findMany({
-        
-    })
-    return habits
-})
+app.register(AppRoutes)
 
 app.listen({port:3333}).then(()=>{
     console.log('Server is on')
